beb6f88c363f26a284959b5229fec1e3fec5f1ce
Hano <hinsul3113@gmail.com>
2019-03-14 00:20:12 -0700

    created Posts component, reducer and action and fetched posts from api, as well as fixed realtime generator issue with babel-polyfill

--- src/index.js
+++ src/index.js
@@ -2,18 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
+import reduxThunk from 'redux-thunk';
 
-import App from './components/app';
 import reducers from './reducers';
+import PostsIndex from './components/PostsIndex'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   reducers,
-  composeEnhancers(applyMiddleware())
+  composeEnhancers(applyMiddleware(reduxThunk))
 );
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Route path="/" component={PostsIndex} />
+      </div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
